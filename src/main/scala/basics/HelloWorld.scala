4ec2fcbf79e04e761173e36525dc8119d300d5d9
Alexandra Walker <awalker3@trinity.edu>
2019-01-22 10:38:30 -0600

    I added an Entity with intersection and edited the Hello World file.

--- src/main/scala/basics/HelloWorld.scala
+++ src/main/scala/basics/HelloWorld.scala
@@ -11,4 +11,18 @@ object HelloWorld {
 	def square(x: Double) = x*x
 	
 	def cube(x: Double) = x*x*x
+
+val name = "Thomas Page"
+var age = 23
+
+val message = name + " is " + age + " years old."
+val message1 =  s"$name is ${age+1} years old"
+
+println(message)
+println(message1)
+
+val square1 = (x:Double) => x*x
+println(square1(3))
+
+val twice: Double => Double = _*2
 }
