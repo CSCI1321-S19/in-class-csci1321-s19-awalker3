a10f5a1ade15c50c1b8eb2dc64b7e291e5731505
Alexandra Walker <awalker3@trinity.edu>
2019-03-17 13:46:32 -0500

    Finished ActorChat with Lewis code

--- src/main/scala/actors/ChatManager.scala
+++ src/main/scala/actors/ChatManager.scala
@@ -1,5 +1,5 @@
 package actors
-
+/*
 import akka.actor.Actor
 import java.io.PrintStream
 import java.io.BufferedReader
@@ -20,4 +20,30 @@ class ChatManager extends Actor {
 object ChatManager {
   case class CheckAllInput
   case class NewChatter(sock: Socket, in: BufferedReader, out: PrintStream, name: String)
+}*/
+
+
+import akka.actor.Actor
+import java.io.PrintStream
+import java.io.BufferedReader
+import java.net.Socket
+import akka.actor.Props
+
+class ChatManager extends Actor {
+  import ChatManager._
+  def receive = {
+    case CheckAllInput =>
+      for (child <- context.children) child ! Chatter.CheckInput
+    case NewChatter(sock, in, out, name) =>
+      context.actorOf(Props(new Chatter(sock, in, out, name)), name)
+    case PrintToAll(message) =>
+      for (child <- context.children) child ! Chatter.PrintMessage(message)
+    case m => println("Oops in ChatManager: " + m)
+  }
+}
+
+object ChatManager {
+  case class NewChatter(sock: Socket, in: BufferedReader, out: PrintStream, name: String)
+  case object CheckAllInput
+  case class PrintToAll(message: String)
 }
